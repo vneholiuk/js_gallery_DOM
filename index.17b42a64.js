bded71e2fadfc0fa03d4b52293b3b946b8180033
Vlados <vnegoluk@gmail.com>
2024-08-07 15:15:11 +0200

    make build

---
+++ index.17b42a64.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var n=e.target.closest(".list-item__link");t.src=n.href});
+//# sourceMappingURL=index.17b42a64.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const thumbs = document.getElementById("thumbs");
+const largeImg = document.getElementById("largeImg");
+thumbs.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const linkElement = e.target.closest(".list-item__link");
+    largeImg.src = linkElement.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
